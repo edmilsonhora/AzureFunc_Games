9911dd52f411d42857a8069ceaf9543c74603f2a
edmilsonhora <edmilson_hora@yahoo.com.br>
2022-04-21 08:04:51 -0300

    Inicial

---
+++ Games_Func.Domain/DataAccess/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Games_Func.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games_Func.Domain.DataAccess
+{
+   public class Contexto :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=GamesDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Record>  Records { get; set; }
+    }
+}
---
+++ Games_Func.Domain/Migrations/20220417210708_Primeiro.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Games_Func.Domain.Migrations
+{
+    public partial class Primeiro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Game = table.Column<string>(type: "TEXT", nullable: true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Pontos = table.Column<int>(type: "INTEGER", nullable: false),
+                    TempoDeJogo = table.Column<string>(type: "TEXT", nullable: true),
+                    Data = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+        }
+    }
+}
